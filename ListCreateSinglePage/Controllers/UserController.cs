2d38c065713b5e39f456d5db22297b040e385f5c
os18 <os18@OS18>
2024-05-15 17:17:36 +0530

    Add project files.

---
+++ ListCreateSinglePage/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ListCreateSinglePage.Models;
+using System.Collections.Generic;
+
+namespace ListCreateSinglePage.Controllers
+{
+    public class UserController : Controller
+    {
+        protected static List<UserModel> users = new List<UserModel>();
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+        [HttpPost]
+        public IActionResult Create(int UserId,string Name,string Email)
+        {
+            if(UserId>0 && !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Email))
+            {
+                UserModel user = new UserModel
+                {
+                    UserId = UserId,
+                    Name = Name,
+                    Email = Email
+
+                };
+                users.Add(user);
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+    }
+}
